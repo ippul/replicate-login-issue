e681733a75cc221bfd952f70c88f60c68f006572
Claudio Luppi <cluppi@redhat.com>
2022-03-08 09:34:07 +0000

    init

--- producer/src/main/java/org/ippul/example/Publish.java
+++ producer/src/main/java/org/ippul/example/Publish.java
@@ -3,8 +3,12 @@ package org.ippul.example;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import javax.jms.*;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Publish {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Publish.class);
+
     public static void main(String[] args) throws Exception {
         final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://ex-aao-hdls-svc:61616", "ippul", "Pa$$w0rd");
         final Connection connection = connectionFactory.createConnection();
@@ -12,11 +16,13 @@ public class Publish {
         final Session session  = connection.createSession();
         final Destination destination = session.createQueue("queue1");
         final MessageProducer producer = session.createProducer(destination);
-        for(int count = 0; count<10_000_000; count ++) {
-            final TextMessage message = session.createTextMessage("Test JMS Message " + UUID.randomUUID().toString());
-            System.out.println("Sending: " + message);
+        for(int count = 0; count < 6; count ++) {
+            final String messageBody = "Test JMS Message " + UUID.randomUUID().toString();
+            final TextMessage message = session.createTextMessage(messageBody);
+            LOGGER.info("Sending message numebr {} with body {}", count, message);
             producer.send(message);
-            Thread.sleep(1000l);
+            LOGGER.info("wait 10s...");
+            Thread.sleep(10000l);
         }
         session.close();
         connection.close();
