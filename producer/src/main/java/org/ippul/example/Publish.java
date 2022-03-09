825425413eb4cd628fc8812ffc1407a9bdcc7b4d
Claudio Luppi <cluppi@redhat.com>
2022-03-09 10:28:09 +0000

    init

--- producer/src/main/java/org/ippul/example/Publish.java
+++ producer/src/main/java/org/ippul/example/Publish.java
@@ -1,6 +1,14 @@
 package org.ippul.example;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.UserSessionRepresentation;
+
 import javax.jms.*;
 import javax.net.ssl.*;
 import java.io.IOException;
@@ -12,20 +20,26 @@ import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.keycloak.OAuth2Constants;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.representations.idm.UserSessionRepresentation;
-
 public class Publish {
     private static final Logger LOGGER = LogManager.getLogger(Publish.class);
 
+    private static final Keycloak KEYCLOAK = KeycloakBuilder.builder()
+            .serverUrl("https://keycloak." + System.getenv("OPENSHIFT_BUILD_NAMESPACE") + ".svc:8443/auth") //KEYCLOAK_SERVICE_HOST
+            .grantType(OAuth2Constants.PASSWORD)
+            .realm("amq-sso-realm")
+            .clientId("admin-cli")
+            .username("admin")
+            .password("Pa$$w0rd")
+            .resteasyClient(
+                    new ResteasyClientBuilder()
+                            .sslContext(getSSLContext("https://keycloak." + System.getenv("OPENSHIFT_BUILD_NAMESPACE") + ".svc:8443/auth"))
+                            .connectionPoolSize(1).build()
+            ).build();
+
     public static void main(String[] args) throws Exception {
+        final String userIdToMonitor = KEYCLOAK.realm("amq-sso-realm")
+                .users().search("ippul").get(0).getId();
+        cleanRHSSOSession(userIdToMonitor);
         final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://ex-aao-hdls-svc:61616", "ippul", "Pa$$w0rd");
         final Connection connection = connectionFactory.createConnection();
         connection.start();
@@ -37,32 +51,29 @@ public class Publish {
             final TextMessage message = session.createTextMessage(messageBody);
             LOGGER.info("Sending message number {} with body {}", count, messageBody);
             producer.send(message);
+            logRHSSOSession(userIdToMonitor);
             LOGGER.info("wait 10 seconds...");
-            logRHSSOSession();
             Thread.sleep(10000l);
         }
         session.close();
         connection.close();
+        LOGGER.info("After run....");
+        logRHSSOSession(userIdToMonitor);
     }
 
-    private static void logRHSSOSession(){
-        Keycloak keycloak = KeycloakBuilder.builder()
-                .serverUrl("https://keycloak.example-1.svc:8443/auth") //KEYCLOAK_SERVICE_HOST
-                .grantType(OAuth2Constants.PASSWORD)
-                .realm("amq-sso-realm")
-                .clientId("admin-cli")
-                .username("admin")
-                .password("Pa$$w0rd")
-                .resteasyClient(
-                        new ResteasyClientBuilder()
-                                .sslContext(getSSLContext("https://keycloak.example-1.svc:8443/auth"))
-                                .connectionPoolSize(10).build()
-                ).build();
-        String userId = keycloak.realm("amq-sso-realm")
-                .users().search("ippul").get(0).getId();
-        List<UserSessionRepresentation> userSessions = keycloak.realm("amq-sso-realm")
-                .users().get(userId).getUserSessions();
-        LOGGER.info("User ippul > Active session {}", userSessions.size());
+    private static void logRHSSOSession(String userIdToMonitor){
+        List<UserSessionRepresentation> userSessions = KEYCLOAK.realm("amq-sso-realm")
+                .users().get(userIdToMonitor).getUserSessions();
+        LOGGER.info("{} active session for user ippul", userSessions.size());
+    }
+
+    private static void cleanRHSSOSession(String userIdToMonitor){
+        List<UserSessionRepresentation> userSessions = KEYCLOAK.realm("amq-sso-realm")
+                .users().get(userIdToMonitor).getUserSessions();
+        for(UserSessionRepresentation userSession : userSessions) {
+            LOGGER.info("Deleting session with id {}", userSession.getId());
+            KEYCLOAK.realm("amq-sso-realm").deleteSession(userSession.getId());
+        }
     }
 
     public static SSLContext getSSLContext(String url) {
